a8be694fc25c848d80f21b28afad60c6a6a01c65
Patrick McLennan <patrick.a.mclennan@gmail.com>
2021-04-12 09:08:25 -0400

    types + codeegen refactor

--- graphql/mutations/user.mutations.ts
+++ graphql/mutations/user.mutations.ts
@@ -11,7 +11,7 @@ const me = {
 export const userMutations = {
   login: (_parent: any, _args: any, _context: any) => me,
   createUser: (_parent: any, { newUser }: any, { db }: { db: Knex }) => {
-    Object.assign(newUser, { role: newUser.email === `patrick.a.mclennan@gmail.com` ? Role.Admin : Role.User });
+    Object.assign(newUser, { role: newUser.email === process.env.ADMIN_EMAIL ? Role.Admin : Role.User });
     return me;
   }
 };
